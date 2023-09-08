087c4d78503e9e925f2a2925cf69a1c37f8756f4
SylviaZartmann <128845497+SylviaZartmann@users.noreply.github.com>
2023-09-08 14:16:03 +0200

    added components + background style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { DotsComponent } from './dots/dots.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    IntroductionComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent,
+    DotsComponent
   ],
   imports: [
     BrowserModule,
