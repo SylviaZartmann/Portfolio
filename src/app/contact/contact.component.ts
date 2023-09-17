479b03a67d7901f663371934c436985ac1cbd0c1
SylviaZartmann <128845497+SylviaZartmann@users.noreply.github.com>
2023-09-17 21:53:51 +0200

    contactform

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,24 +1,49 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
-  myForm: FormGroup;
+  FormData = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    message: ['', Validators.required],
+  });
+  isSubmitted = false;
 
-  
-  constructor(private fb: FormBuilder) {
-    this.myForm = this.fb.group({
-      name: '',
-      email: '',
-      message: '',
-    });
-  }
+  constructor(private fb: FormBuilder) {}
 
   onSubmit() {
-    console.log(this.myForm.value);
+    this.isSubmitted = true;
+    this.sendMail();
+  }
+
+  async sendMail() {
+    const sendData = {
+    name: this.FormData.controls.name.value,
+    email: this.FormData.controls.email.value,
+    message: this.FormData.controls.message.value
+  };
+  await fetch('https://sylvia-zartmann.developerakademie.net/send_mail/send_mail.php', {
+    method: 'POST',
+    body: JSON.stringify(sendData)
+  });
+  }
+
+  validateInput(whichInput: string) {
+    this.FormData.get(whichInput)?.invalid &&
+      (this.FormData.get(whichInput)?.dirty ||
+        this.FormData.get(whichInput)?.touched ||
+        this.isSubmitted);
+  }
+  validateErrorText(whichInput: string) {
+    return (
+      this.FormData.get(whichInput)?.hasError('required') &&
+      (this.FormData.get(whichInput)?.dirty ||
+        this.isSubmitted)
+    );
   }
 }
