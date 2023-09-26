c17bcc5a6453c9f66c905a994f4d874c63710da9
SylviaZartmann <128845497+SylviaZartmann@users.noreply.github.com>
2023-09-26 10:55:50 +0200

    animations

--- src/app/sample/sample.component.ts
+++ src/app/sample/sample.component.ts
@@ -1,78 +1,98 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Component, Renderer2, ElementRef, ViewChild, OnInit } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import {
+  Component,
+  Renderer2,
+  ElementRef,
+  ViewChild,
+  OnInit,
+} from '@angular/core';
 
-const slideInOut = trigger('slideInOut', [
-  state(
-    'start', 
-    style ({
-      right: '0',
-      left: 'unset'
-  })
+ const slideInOut = 
+trigger('slideInOut', [
+  state('start', style({
+    left: '0',
+    right: 'unset',
+    })
   ),
-  state(
-    'end', 
-    style ({
+  state('end', style({
+    right: '0',
+    left: 'unset',
+    })
+  ),
+  transition('end <=> start', [animate('1s ease-in-out')]),
+  //transition('start => end', [animate('1s ease-in')]),
+ ]);
+
+const slideInOutreverse = 
+trigger('slideInOutreverse', [
+  state('start', style({
       left: '0',
-      right: 'unset'
-  })
+      right: 'unset',
+    })
   ),
-  transition('start => *', [animate('1s ease-out')]),
-  transition('* => start', [animate('1s ease-in')])
-]);
-const slideInOutreverse = trigger('slideInOutreverse', [
-  state(
-    'start', 
-    style ({
-      left: '0',
-      right: 'unset'
-  })
-  ),
-  state(
-    'end', 
-    style ({
+  state('end', style({
       right: '0',
-      left: 'unset'
-  })
+      left: 'unset',
+    })
   ),
   transition('start => end', [animate('1s ease-out')]),
-  transition('* => start', [animate('1s ease-in')])
-])
-
+  transition('end => start', [animate('1s ease-in')]),
+]);
 
 @Component({
   selector: 'app-sample',
   templateUrl: './sample.component.html',
   styleUrls: ['./sample.component.scss'],
-  animations: [slideInOut, slideInOutreverse]
+  animations: [slideInOut, slideInOutreverse],
 })
-
 export class SampleComponent implements OnInit {
   @ViewChild('elementRef1', { static: true }) elementRef1: ElementRef | any;
+  // @ViewChild('elementRef2', { static: true }) elementRef2: ElementRef | any;
+  // @ViewChild('elementRef3', { static: true }) elementRef3: ElementRef | any;
 
-  isSlided = false;
-  inView = false;
+  showText1 = false;
+  showText2 = false;
+  showText3 = false;
 
   private observer: IntersectionObserver | undefined;
 
   constructor(private renderer: Renderer2) {}
-  
+
   ngOnInit() {
+    this.observer = new IntersectionObserver(
+      this.handleIntersection.bind(this),
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 1,
+      }
+    );
     
-    this.observer = new IntersectionObserver(this.handleIntersection.bind(this), {
-      rootMargin: '0px',
-      threshold: 1
-    });
-  
+
     this.observer.observe(this.elementRef1.nativeElement);
-  }
+    // this.observer.observe(this.elementRef2.nativeElement);
+    // this.observer.observe(this.elementRef3.nativeElement);
+
+   }
+
+
 
   private handleIntersection(entries: IntersectionObserverEntry[]) {
+    console.log(entries);
     if (entries[0].isIntersecting) {
-      this.inView = true;
-      this.isSlided = true;
+      this.showText1 = true;
+      this.showText2 = false;
+      this.showText3 = false;
     } else {
-      this.inView = false;
-      this.isSlided = false;
+      this.showText1 = false;
+      this.showText2 = false;
+      this.showText3 = false;
     }
   }
 }
