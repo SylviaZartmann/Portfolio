1f2f22ecc1fb1b0cdd12ee25dccc8526bbef78bb
SylviaZartmann <128845497+SylviaZartmann@users.noreply.github.com>
2023-09-27 14:38:38 +0200

    page animation

--- src/app/introduction/introduction.component.ts
+++ src/app/introduction/introduction.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import {
   animate,
   state,
@@ -29,7 +35,7 @@ const arrow = trigger('arrow', [
   state(
     'start',
     style({
-      transform:'translate(-80px, 80px)'
+      transform: 'translate(-80px, 80px)',
     })
   ),
   transition('start <=> *', [animate('0.5s ease-in-out')]),
@@ -43,10 +49,11 @@ const arrow = trigger('arrow', [
 })
 export class IntroductionComponent implements OnInit {
   @ViewChild('elementRef1', { static: true }) elementRef1: ElementRef | any;
+  @ViewChild('elementRef2', { static: true }) elementRef2: ElementRef | any;
 
-  showSomething = false;
+  showSomething1 = false;
+  showSomething2 = false;
   animation: string | undefined;
-
 
   private observer: IntersectionObserver | undefined;
 
@@ -63,14 +70,19 @@ export class IntroductionComponent implements OnInit {
     );
 
     this.observer.observe(this.elementRef1.nativeElement);
+    this.observer.observe(this.elementRef2.nativeElement);
   }
 
   private handleIntersection(entries: IntersectionObserverEntry[]) {
     entries.forEach((entry) => {
-      this.showSomething =
+      this.showSomething1 =
         entry.target.getAttribute('id') == 'elementRef1'
           ? entry.isIntersecting
-          : this.showSomething;
-  })
-}
+          : this.showSomething1;
+      this.showSomething2 =
+        entry.target.getAttribute('id') == 'elementRef2'
+          ? entry.isIntersecting
+          : this.showSomething2;
+    });
+  }
 }
